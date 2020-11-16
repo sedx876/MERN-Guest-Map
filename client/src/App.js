fb7f6982f784721a6b0e670916258a5febfd1b78
sedx876 <swatkins876@gmail.com>
2020-11-16 11:39:41 -0500

    added API.js and MessageCardForm.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,76 +1,178 @@
 import React, { Component } from 'react';
-import { TileLayer, Marker, Popup, MapContainer } from 'react-leaflet'
-import './styles/App.css'
-import userLocationURL from './user_location.svg';
-//import messageLocationURL from './message_location.svg';
 import L from 'leaflet';
-//import { Card, CardText, Button } from 'reactstrap';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { Card, CardText, Button } from 'reactstrap';
+
+import userLocationURL from './user_location.svg';
+import messageLocationURL from './message_location.svg';
+
+import MessageCardForm from './components/MessageCardForm';
+import { getMessages, getLocation, sendMessage } from './components/API';
+
+import './styles/App.css';
 
 const myIcon = L.icon({
   iconUrl: userLocationURL,
-  iconSize: [50, 82],
-  iconAnchor: [22, 94],
-  popupAnchor: [0, -75],
+  iconSize: [50, 82]
 });
 
-// const messageIcon = L.icon({
-//   iconUrl: messageLocationURL,
-//   iconSize: [50, 82]
-// });
-
-
-
+const messageIcon = L.icon({
+  iconUrl: messageLocationURL,
+  iconSize: [50, 82]
+});
 
 class App extends Component {
-
   state = {
     location: {
       lat: 51.505,
       lng: -0.09,
     },
     haveUsersLocation: false,
-    zoom: 13,
+    zoom: 2,
+    userMessage: {
+      name: '',
+      message: ''
+    },
+    showMessageForm: false,
+    sendingMessage: false,
+    sentMessage: false,
+    messages: []
   }
 
   componentDidMount() {
-    navigator.geolocation.getCurrentPosition((position) => {
+    getMessages()
+      .then(messages => {
+        this.setState({
+          messages
+        });
+      });
+  }
+
+  showMessageForm = () => {
+    this.setState({
+      showMessageForm: true
+    });
+    getLocation()
+    .then(location => {
       this.setState({
-        location: {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude
-        }
+        location,
+        haveUsersLocation: true,
+        zoom: 13
       });
     });
   }
 
-  render(){
-    const position = [this.state.location.lat, this.state.location.lng]
-  return (
-    <MapContainer 
-    className="map" 
-    center={position} 
-    zoom={13} 
-    scrollWheelZoom={false}>
-    <TileLayer
-      attribution='&copy; 
-      <a href="http://osm.org/copyright">Guest Map</a> contributors'
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    />
-    { this.state.haveUsersLocation ? 
+  cancelMessage = () => {
+    this.setState({
+      showMessageForm: false
+    });
+  }
+
+  formIsValid = () => {
+    let { name, message } = this.state.userMessage;
+    name = name.trim();
+    message = message.trim();
+
+    const validMessage =
+      name.length > 0 && name.length <= 500 &&
+      message.length > 0 && message.length <= 500;
+
+    return validMessage && this.state.haveUsersLocation ? true : false;
+  }
+
+  formSubmitted = (event) => {
+    event.preventDefault();
+    
+    if (this.formIsValid()) {
+      this.setState({
+        sendingMessage: true
+      });
+
+      const message = {
+        name: this.state.userMessage.name,
+        message: this.state.userMessage.message,
+        latitude: this.state.location.lat,
+        longitude: this.state.location.lng,
+      };
+
+      sendMessage(message)
+        .then((result) => {
+          setTimeout(() => {
+            this.setState({
+              sendingMessage: false,
+              sentMessage: true
+            });
+          }, 4000);
+        });
+    }
+  }
+
+  valueChanged = (event) => {
+    const { name, value } = event.target;
+    this.setState((prevState) => ({
+      userMessage: {
+        ...prevState.userMessage,
+        [name]: value
+      }
+    }))
+  }
+
+  render() {
+    const position = [this.state.location.lat, this.state.location.lng];
+    return (
+      <div className="map">
+        <MapContainer
+          className="map"
+          worldCopyJump={true}
+          center={position}
+          zoom={this.state.zoom}>
+          <TileLayer
+            attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors and Chat location by Iconika from the Noun Project"
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          {
+            this.state.haveUsersLocation ? 
             <Marker
               position={position}
               icon={myIcon}>
-            </Marker> : '' }
-    <Marker 
-    position={position}
-    icon={myIcon}>
-      <Popup>
-        A pretty CSS3 popup. <br /> Easily customizable.
-      </Popup>
-    </Marker>
-  </MapContainer>
-  );
- }
+            </Marker> : ''
+          }
+          {this.state.messages.map(message => (
+            <Marker
+              key={message._id}
+              position={[message.latitude, message.longitude]}
+              icon={messageIcon}>
+              <Popup>
+                <p><em>{message.name}:</em> {message.message}</p>
+                { message.otherMessages ? message.otherMessages.map(message => <p key={message._id}><em>{message.name}:</em> {message.message}</p>) : '' }
+              </Popup>
+            </Marker>
+          ))}
+        </MapContainer>
+        {
+          !this.state.showMessageForm ?
+          <Button className="message-form" onClick={this.showMessageForm} color="info">Add a Message</Button> :
+          !this.state.sentMessage ?
+          <MessageCardForm
+            cancelMessage={this.cancelMessage}
+            showMessageForm={this.state.showMessageForm}
+            sendingMessage={this.state.sendingMessage}
+            sentMessage={this.state.sentMessage}
+            haveUsersLocation={this.state.haveUsersLocation}
+            formSubmitted={this.formSubmitted}
+            valueChanged={this.valueChanged}
+            formIsValid={this.formIsValid}
+          /> :
+          <Card body className="thanks-form">
+            <CardText>Thanks for submitting a message!</CardText>
+          </Card>
+        }
+        <Card className="footer">
+          <CardText> Made with <span role="img" aria-label="love">💚</span> by <a href="https://github.com/sedx876" target="_blank" rel="noopener noreferrer">Sedx876</a></CardText>
+        </Card>
+      </div>
+    );
+  }
 }
 
 export default App;
